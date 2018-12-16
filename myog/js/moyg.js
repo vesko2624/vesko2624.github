9eefa7caa719c1a99a6975c2b697cdd191f0a339
vesko2624 <vesko_g@hotmail.com>
2018-12-16 23:08:50 +0200

    Промени

--- myog/js/moyg.js
+++ myog/js/moyg.js
@@ -1,26 +1,40 @@
+let se = new Audio("sounds/ButtonClick.wav");
+let adr = "sounds/BG/",
+	playlist = ["bakura","banditkeith","critiasJ","exodia","joey","kaiba","millenniumbattle","obelisk","orichalcos","passionateduelistextended","ra","slifer","timaeus","yugi","zorc"],
+	ras = ".mp3",
+	played = new Audio(),
+	current_song = -1;
+played.volume = 0.3;
+if(played.canPlayType('audio/mp3') !== "probably") ras = ".ogg";
 
-var se = new Audio();
-se.src = "sounds/ButtonClick.wav";
-adr="sounds/BG/";
-playlist = ["bakura","banditkeith","critiasJ","exodia","joey","kaiba","millenniumbattle","obelisk","orichalcos","passionateduelistextended","ra","slifer","timaeus","yugi","zorc"];
-ras=".mp3";
-played = new Audio();
+async function rand_song(){
+	let missing = 1;
+	let next_song = Math.floor(Math.random() * 15);
+	if(current_song == -1 || current_song != next_song) current_song = next_song;
+	else {
+		function randomize(){
+			return new Promise((resolve, reject) => {
+				let interval = setInterval(() => {
+					next_song = Math.floor(Math.random() * 15);
+					if(next_song != current_song){
+						clearInterval(interval);
+						resolve(next_song);
+					}
+				}, 10);
+			});
+		}
+		current_song = await randomize();
+	}
+	return playlist[current_song];
 
-function BGA(){
-	played.src = adr + playlist[Math.floor(Math.random() * 15)] + ras;
-	played.volume = 0.3;
-	played.play();
-	played.onended = BGA;
 }
-
-/*window.onerror = function() {
-  var message = " error";
-  alert(message);
-  return true;
-};*/
+async function BGA(){
+	played.src = adr + await rand_song() + ras;
+	played.play();
+}
+played.onended = BGA;
 
 function btt(url) {
 	se.play();
-	url += ".html";
-    setTimeout(function(){ window.location.href = url; }, 500);
+    setTimeout(function(){ window.location.href = `${url}.html`; }, 500);
 }
